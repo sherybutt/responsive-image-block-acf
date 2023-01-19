5c9ad29a9346fc7a93dc763da6fcc52896683849
Sheryar Butt <73537344+sherybutt@users.noreply.github.com>
2023-01-19 12:43:58 +0500

    Update responsive-image.php

--- Responsive Image Block - ACF/Templates Code/block file code/responsive-image.php
+++ Responsive Image Block - ACF/Templates Code/block file code/responsive-image.php
@@ -9,10 +9,14 @@
 $desktopImage = get_field('desktop-image');
 $altTag = get_field('alt-tag-name');
 $className = get_field('css_class');
+$linkToImage = get_field('link_to_image');
 
 ?>
 
-<picture <?php if($className){ ?> class="<?php echo $className; ?>" <?php  } ?> >
+
+<picture <?php if($className){ ?> class="wps-imgBlock <?php echo $className; ?>" <?php  } ?> >
+
+
   
   <?php
   if( have_rows('responsive_image_fields') ){
@@ -26,5 +30,23 @@ $className = get_field('css_class');
   } //end of if 
   ?>
   <img src="<?php echo $desktopImage; ?>" <?php if($altTag){ ?> alt="<?php echo $altTag; ?>" <?php } ?> />
-  
+
+
+  <?php if($linkToImage) { ?>  <a href="<?php echo $linkToImage ?>"></a> <?php } ?>
+
 </picture>
+
+
+<style>
+.wps-imgBlock {
+  position: relative;
+  display: block;
+}
+.wps-imgBlock a {
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
+}
+</style>
